7a161d52646e09d3e280fb90e91a6252bf216ec4
digimeera <meeradigi92@gmail.com>
2025-02-25 12:15:30 +0530

    Add my project

--- main.tf
+++ main.tf
@@ -1,31 +1,83 @@
-# Create an S3 Bucket
-resource "aws_s3_bucket" "tf-testing-news3-bucket-unique-name" {
-  bucket = var.bucket_name
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-# Set bucket ownership controls (optional but recommended)
-resource "aws_s3_bucket_ownership_controls" "ownership" {
-  bucket = aws_s3_bucket.tf-testing-news3-bucket-unique-name.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict in production
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Apply Public ACL settings (disable if bucket should be private)
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.tf-testing-news3-bucket-unique-name.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+resource "aws_instance" "web" {
+  ami           = "ami-05b10e08d247fb927" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  security_groups = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
 
-# Upload a file to S3 bucket
-resource "aws_s3_object" "upload_file" {
-  bucket = aws_s3_bucket.tf-testing-news3-bucket-unique-name.id
-  key    = "uploaded-file.txt"  # S3 object key (filename)
-  source = "local-file.txt"     # Local file to upload
-  acl    = "public-read"        # Change to "private" if needed
-}
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras enable nginx1
+              sudo yum install -y nginx unzip
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
 
+              cd /usr/share/nginx/html
+              sudo rm -rf *
+              sudo curl -o little-fashion.zip -L "https://www.free-css.com/assets/files/free-css-templates/download/page296/little-fashion.zip"
+              sudo unzip little-fashion.zip
+              sudo mv little-fashion-html/* .
+              sudo rm -rf little-fashion.zip little-fashion-html
+              EOF
+
+  tags = {
+    Name = "LittleFashionWeb"
+  }
+}
 
--- output.tf
+++ output.tf
@@ -1,8 +1,3 @@
-
-output "bucket_name" {
-  value = aws_s3_bucket.tf-testing-news3-bucket-unique-name.id
-}
-
-output "file_url" {
-  value = "https://${aws_s3_bucket.tf-testing-news3-bucket-unique-name.bucket}.s3.amazonaws.com/${aws_s3_object.upload_file.key}"
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
